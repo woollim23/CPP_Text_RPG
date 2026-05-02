01d63d0d0b05e5c67ddd526059662466ce025b3c
woollim23 <begin2125@gmail.com>
2026-05-02 21:09:05 +0900

    update step2

--- CPP_Text_RPG/main.cpp
+++ CPP_Text_RPG/main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void printStatus(string name, int stat[])
 {
-	cout << "====================================" << endl;
+	cout << endl << "====================================" << endl;
 	cout << "  " << name << " 의 현재 능력치" << endl;
 	cout << "====================================" << endl;
 	cout << "HP: " << stat[0] << "   MP: " << stat[1] << endl;
@@ -23,10 +23,20 @@ int main()
 	cin >> name;
 
 	int stat[4] = { 0, }; // 0 - HP, 1 - MP, 2 - 공격력, 3 - 방어력 
-	cout << "HP와 MP를 입력해주세요: ";
-	cin >> stat[0] >> stat[1];
-	cout << "공격력과 방어력을 입력해주세요: ";
-	cin >> stat[2] >> stat[3];
+	while (true)
+	{
+		cout << endl << "HP와 MP를 입력해주세요: ";
+		cin >> stat[0] >> stat[1];
+		if (stat[0] > 50 && stat[1] > 50)
+			break;
+	}
+	while (true)
+	{
+		cout << endl << "공격력과 방어력을 입력해주세요: ";
+		cin >> stat[2] >> stat[3];
+		if (stat[2] > 50 && stat[3] > 50)
+			break;
+	}
 
 	printStatus(name, stat);
 	return 0;
