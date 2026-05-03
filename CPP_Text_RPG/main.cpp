8532b340f825ffe396c0fa9f466567f3be60fa70
woollim23 <begin2125@gmail.com>
2026-05-03 10:25:07 +0900

    update step3

--- CPP_Text_RPG/main.cpp
+++ CPP_Text_RPG/main.cpp
@@ -39,5 +39,54 @@ int main()
 	}
 
 	printStatus(name, stat);
+
+	cout << "* HP 포션 5개, MP 포션 5개가 기본 지급되었습니다." << endl;
+	cout << "============================================" << endl;
+	cout << "< 캐릭터 강화 >" << endl;
+	cout << "============================================" << endl;
+	cout << "1. HP UP    2. MP UP    3. 공격력 2배" << endl;
+	cout << "4. 방어력 2배  5. 현재 능력치  0. 게임 시작" << endl;
+
+	int HP = 5;
+	int MP = 5;
+	bool isGameStart = false;
+
+	while (!isGameStart)
+	{
+		int num;
+		cout << "번호를 선택해주세요: ";
+		cin >> num;
+
+		switch (num)
+		{
+		case 0:
+			cout << "게임을 시작합니다!" << endl;
+			isGameStart = true;
+			break;
+		case 1:
+			stat[0] += 20;
+			HP -= 1;
+			cout << "* HP가 20 증가했습니다. (HP 포션 차감: 남은 포션 " << HP << "개)"<<endl;
+			break;
+		case 2:
+			stat[1] += 20;
+			MP -= 1;
+			cout << "* MP가 20 증가했습니다. (MP 포션 차감: 남은 포션 " << MP << "개)"<<endl;
+			break;
+		case 3:
+			stat[2] *= 2;
+			cout << "* 공격력이 2배 증가했습니다." << endl;
+			break;
+		case 4:
+			stat[3] *= 2;
+			cout << "* 방어력이 2배 증가했습니다." << endl;
+			break;
+		case 5:
+			printStatus(name, stat);
+			break;
+		default:
+			cout << "잘못된 번호를 입력했습니다" << endl;
+		}
+	}
 	return 0;
 }
